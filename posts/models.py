6b22e37cf8c462529bb6ffd4506e2063d4b9941d
ChegeDaniella <chegedaniella@gmail>
2020-05-29 12:54:19 +0300

    changing fiels to take images

--- posts/models.py
+++ posts/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    profile_photo = models.CharField(max_length = 30)
+    profile_photo = models.ImageField(upload_to = 'saved/')
     bio = models.CharField(max_length = 30)
 
 class Post(models.Model):
-    image = models.CharField(max_length=30)
+    image = models.ImageField(upload_to = 'saved/')
     image_name = models.CharField(max_length = 50)
     image_caption = models.CharField(max_length = 600)
     image_profile = models.ForeignKey(Profile,on_delete=models.CASCADE)

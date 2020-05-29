c9eac6575cad0b006361fa7bf4736e6173218e16
ChegeDaniella <chegedaniella@gmail>
2020-05-29 12:42:17 +0300

    creating tables

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    image = models.CharField(max_length=30)
+    image_name = models.CharField(max_length = 50)
+    image_caption = models.CharField(max_length = 600)
+    # image_profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    
+

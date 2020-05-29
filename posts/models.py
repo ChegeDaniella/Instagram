0e8281510459c47a13a5c42f84db10d9d0aacad4
ChegeDaniella <chegedaniella@gmail>
2020-05-29 12:50:10 +0300

    creating a databse again

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.CharField(max_length = 30)
+    bio = models.CharField(max_length = 30)
+
 class Post(models.Model):
     image = models.CharField(max_length=30)
     image_name = models.CharField(max_length = 50)
     image_caption = models.CharField(max_length = 600)
-    # image_profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    image_profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
     
 

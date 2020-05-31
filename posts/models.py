c4d0efa08a9619235b5cfa7f4c71c015ca67d471
ChegeDaniella <chegedaniella@gmail>
2020-06-01 02:19:48 +0300

    adding likes model

--- posts/models.py
+++ posts/models.py
@@ -45,6 +45,18 @@ class Post(models.Model):
     def delete_post(self):
         post = self.object.filter(pk=id).delete()
         return post    
+LIKE_CHOICES = (
+    ('Like','Like'),
+    ('Unlike','Unlike')
+)
+
+class Like(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES, default='Like', max_length=10)
+
+    def __str__(self):
+        return self.value
 
     
 class Comments(models.Model):
